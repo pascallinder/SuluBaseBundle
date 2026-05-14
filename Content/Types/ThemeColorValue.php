a1a83f8321f9aec4dcb0bef53bc148f082257728
pascallinder <pascal.linder97@hotmail.com>
2026-05-14 11:38:32 +0200

    dark mode aware

---
+++ Content/Types/ThemeColorValue.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Linderp\SuluBaseBundle\Content\Types;
+
+final class ThemeColorValue
+{
+    public function __construct(
+        private string $light = 'inherit',
+        private ?string $dark = null
+    ) {
+    }
+
+    public function getLight(): string
+    {
+        return $this->light;
+    }
+
+    public function getDark(): string
+    {
+        return $this->dark ?: $this->light;
+    }
+
+    public function __toString(): string
+    {
+        return $this->light;
+    }
+}
--- Resources/js/src/components/content/types/ColorPickerCustom/ColorPickerCustom.js
+++ Resources/js/src/components/content/types/ColorPickerCustom/ColorPickerCustom.js
@@ -6,6 +6,10 @@ class ColorPickerCustom extends React.PureComponent {
     constructor() {
         super();
         this.handleChange = this.handleChange.bind(this);
+        this.handleModeChange = this.handleModeChange.bind(this);
+        this.state = {
+            activeMode: 'light',
+        };
         if (process.env.COLOR_PICKER_COLORS) {
 
             this.colorlist = process.env.COLOR_PICKER_COLORS.split(' ');
@@ -15,47 +19,126 @@ class ColorPickerCustom extends React.PureComponent {
         }
     }
 
-    handleChange(value) {
+    getNormalizedValue() {
+        const { value } = this.props;
+
+        if (value && typeof value === 'object' && !Array.isArray(value)) {
+            return {
+                light: value.light || 'inherit',
+                dark: value.dark ?? null,
+            };
+        }
+
+        if (typeof value === 'string' && value.length > 0) {
+            return {
+                light: value,
+                dark: null,
+            };
+        }
+
+        return {
+            light: 'inherit',
+            dark: null,
+        };
+    }
+
+    getDisplayedValue(mode) {
+        const value = this.getNormalizedValue();
+
+        if (mode === 'dark') {
+            return value.dark ?? value.light;
+        }
+
+        return value.light;
+    }
+
+    handleChange(mode, value) {
         if (this.props.disabled) {
             return;
         }
-        this.props.onChange(value);
+
+        const nextValue = this.getNormalizedValue();
+        nextValue[mode] = value;
+
+        this.props.onChange(nextValue);
         this.props.onFinish();
     }
 
+    handleModeChange(mode) {
+        this.setState({
+            activeMode: mode,
+        });
+    }
+
     componentDidMount() {
-        const radiogroup = document.getElementsByClassName("color-picker-radiogroup");
-        const radioArray = [];
-        for (let i = 0; i < radiogroup.length; i++) {
-            let label = radiogroup[i].children;
-            radioArray.push(label);
-            radiogroup[i].parentElement.parentElement.parentElement.classList.add("radio-container")
-            for (let j = 0; j < radioArray[i].length; j++) {
-                const element = radioArray[i][j].children[0];
-                element.style.backgroundColor = this.colorlist[j]
-                if(this.colorlist[j] === 'inherit') {
+        this.decorateRadioGroups();
+    }
+
+    componentDidUpdate() {
+        this.decorateRadioGroups();
+    }
+
+    decorateRadioGroups() {
+        const radiogroups = document.getElementsByClassName('color-picker-radiogroup');
+        for (let i = 0; i < radiogroups.length; i++) {
+            const labels = radiogroups[i].children;
+            radiogroups[i].parentElement.parentElement.parentElement.classList.add('radio-container');
+            for (let j = 0; j < labels.length; j++) {
+                const element = labels[j].children[0];
+                if (!element) {
+                    continue;
+                }
+
+                element.style.backgroundColor = this.colorlist[j];
+                if (this.colorlist[j] === 'inherit') {
                     element.classList.add('inherited-color');
+                } else {
+                    element.classList.remove('inherited-color');
                 }
             }
         }
     }
 
     render() {
-        const { value: value, disabled } = this.props;
+        const { disabled } = this.props;
+        const { activeMode } = this.state;
         return (
-            <RadioGroup
-                value={value}
-                onChange={this.handleChange}
-                className={`color-picker-radiogroup${disabled ? ' is-disabled' : ''}`}
-            >
-                {this.colorlist.map((color, index) => (
-                    <Radio value={color} key={index} />
-                ))}
-            </RadioGroup>
+            <div className={`color-picker-custom${disabled ? ' is-disabled' : ''}`}>
+                <div className="color-picker-custom__toolbar">
+                    <div className="color-picker-custom__switch" role="tablist" aria-label="Theme mode">
+                        <button
+                            type="button"
+                            className={`color-picker-custom__switch-button${activeMode === 'light' ? ' is-active' : ''}`}
+                            onClick={() => this.handleModeChange('light')}
+                        >
+                            Light
+                        </button>
+                        <button
+                            type="button"
+                            className={`color-picker-custom__switch-button${activeMode === 'dark' ? ' is-active' : ''}`}
+                            onClick={() => this.handleModeChange('dark')}
+                        >
+                            Dark
+                        </button>
+                    </div>
+                </div>
+                <RadioGroup
+                    value={this.getDisplayedValue(activeMode)}
+                    onChange={(value) => this.handleChange(activeMode, value)}
+                    className="color-picker-radiogroup"
+                >
+                    {this.colorlist.map((color, index) => (
+                        <Radio value={color} key={`${activeMode}-${index}`} />
+                    ))}
+                </RadioGroup>
+            </div>
         );
     }
 }
 ColorPickerCustom.defaultProps = {
-    value: 'inherit',
+    value: {
+        light: 'inherit',
+        dark: null,
+    },
 };
 export default ColorPickerCustom;
